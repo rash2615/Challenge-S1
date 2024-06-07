0d1b5eb19e99ee6306618516275a3420c3393031
Rashmi <rashmi.bhavsar02@gmail.com>
2024-06-07 18:40:54 +0200

    Integration Accueil-Connexion-Inscription

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    // Directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // Public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // Only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/app.js')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // Will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // Enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // Enables @babel/preset-env polyfills
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3,
+    })
+
+    // Enables Sass/SCSS support
+    //.enableSassLoader()
+
+    // Uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // Uncomment if you use React
+    //.enableReactPreset()
+
+    // PostCSS support
+    .enablePostCssLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
