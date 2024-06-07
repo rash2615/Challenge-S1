762913f0fab6ecc8b8a0418af74f84f2e8458d49
LeGrizzly <xalsie.ff@hotmail.fr>
2024-06-07 16:30:58 +0200

    chore: CRUD user roles and form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -12,11 +14,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
-            ->add('role')
+            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', TextType::class)
+            ->add('password', TextType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => ['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_USER' => 'ROLE_USER', 'ROLE_CUSTOMER' => 'ROLE_CUSTOMER'],
+                'expanded' => true,
+                'multiple' => true,
+            ]
+        )
         ;
     }
 
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,41 +3,61 @@
 namespace App\Repository;
 
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
- * @extends ServiceEntityRepository<User>
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    /**
+     * Delete all the unconfirmed accounts of the database.
+     */
+    public function clearUnconfirmedAccounts(DateTime $dateTime): array
+    {
+        $unconfirmedAccounts = $this->createQueryBuilder('u')
+            ->where('u.createdAt < :dateTime')
+            ->setParameter('dateTime', $dateTime)
+            ->andWhere('u.confirmedAt IS NULL')
+            ->getQuery()
+            ->getResult();
+
+        if (!empty($unconfirmedAccounts)) {
+            foreach ($unconfirmedAccounts as $unconfirmedAccount) {
+                $this->_em->remove($unconfirmedAccount);
+                $this->_em->flush();
+            }
+
+            return [true, $unconfirmedAccounts];
+        }
+
+        return [false, []];
+    }
 }
